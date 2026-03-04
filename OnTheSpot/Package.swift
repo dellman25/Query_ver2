b80902c98bf48253ff248f64a38b66923ecdc19f
Yazin <github-7itbcg5@yaz.in>
2026-03-04 14:11:50 +0300

    Add FluidAudio SPM dependency for on-device transcription

--- OnTheSpot/Package.swift
+++ OnTheSpot/Package.swift
@@ -5,9 +5,15 @@ import PackageDescription
 let package = Package(
     name: "OnTheSpot",
     platforms: [.macOS(.v26)],
+    dependencies: [
+        .package(url: "https://github.com/FluidInference/FluidAudio.git", from: "0.7.9"),
+    ],
     targets: [
         .executableTarget(
             name: "OnTheSpot",
+            dependencies: [
+                .product(name: "FluidAudio", package: "FluidAudio"),
+            ],
             path: "Sources/OnTheSpot",
             exclude: ["Info.plist", "OnTheSpot.entitlements", "Assets"]
         ),
